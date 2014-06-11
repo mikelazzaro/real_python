51e73120a16e76ffdd63cac8d4d0bd9f4555c954
Mike Lazzaro <mlazzaro@potentiasys.com>
2014-06-11 14:22:44 -0500

    Added vim swap to ,gitignore, finished chapter 5

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 # ---- Flask Hello World ----
 
+from __future__ import print_function
+
+import sys
+
 from flask import Flask
 
 # Create app object
 app = Flask(__name__)
 
+# Turn on debugging
+app.config["DEBUG"] = True
+
 # define view using function, which returns string
     # Use decorators to link function to url
-@app.route("/")
 @app.route("/hello")
+@app.route("/")
 def hello_world():
-    return "Hello, world!"
+    return "Hello, world!i?!?!?!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+#    return "Search"
+    return search_query
 
 # start the dev server using the run() method
 
 if __name__ == "__main__":
+#    app.run(debug=True)
     app.run()
+    sys.exit(0)
 
