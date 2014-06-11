35aac96ae30a9e07fb2017c5477efc6458e1f251
Mike Lazzaro <mlazzaro@potentiasys.com>
2014-06-11 15:08:41 -0500

    Homework 5-1

--- sql/sqla.py
+++ sql/sqla.py
@@ -8,10 +8,15 @@ conn = sqlite3.connect("new.db")
 # Get cursor
 cursor = conn.cursor()
 
+# Drop table if it exists
+cursor.execute("""
+                DROP TABLE IF EXISTS population
+                """)
+
 # Create table
 cursor.execute("""
                 CREATE TABLE population
-                    (city TEXT, state TEXT, population, INT)
+                    (city TEXT, state TEXT, population INT)
                 """)
 
 # Close the database
